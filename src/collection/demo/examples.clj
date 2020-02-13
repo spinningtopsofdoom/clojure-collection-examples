ed2ebd924a1892bc6d56ee1ecbb089a371384a14
Peter Schuck <peter@bendyworks.com>
2020-02-12 20:52:03 -0600

    Basic Vector functionality

--- src/collection/demo/examples.clj
+++ src/collection/demo/examples.clj
@@ -1,1 +1,16 @@
 (ns collection.demo.examples)
+
+;; Vectors
+[:this :is :a :vector]
+
+;; Vectors are functions
+([:duck :duck :goose :duck] 2)
+;; => :goose
+([:swan :parrot :falcon :flamingo] 1)
+;; => :parrot
+
+;; Using an out of bounds throws an exception
+([:swan :parrot :falcon :flamingo] -1)
+;; => IndexOutOfBoundsException
+([:swan :parrot :falcon :flamingo] 5)
+;; => IndexOutOfBoundsException
