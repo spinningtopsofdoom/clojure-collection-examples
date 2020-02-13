a313cfdd6ce5696897b976d6326be602bb5ff046
Peter Schuck <peter@bendyworks.com>
2020-02-12 21:14:39 -0600

    Map function examples

--- src/collection/demo/examples.clj
+++ src/collection/demo/examples.clj
@@ -14,3 +14,26 @@
 ;; => IndexOutOfBoundsException
 ([:swan :parrot :falcon :flamingo] 5)
 ;; => IndexOutOfBoundsException
+
+;; Maps
+{:a 0 :clojure 1 :key 2 :value 3 :map 4}
+
+;; Maps are also functions
+
+({:peanuts 4 :walnuts 6 :jelly-beans 2} :peanuts)
+;; => 4
+
+({:north :grue :south :treasure :east :pit} :east)
+;; => :pit
+
+;; For keys not in the map nil is returned
+({:north :grue :south :treasure :east :pit} :west)
+;; => nil
+
+;; Adding a second parameter (:food) set a non found value
+({:north :grue :south :treasure :east :pit} :west :food)
+;; => :food
+({} :key :not-found)
+;; => :not-found
+({:north :grue :south :treasure :east :pit} :east :food)
+;; => :pit
