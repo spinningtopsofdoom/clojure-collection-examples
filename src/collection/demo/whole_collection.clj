78a6e05c948c7f7ab4a65513df8d824c5b88dcbc
Peter Schuck <peter@bendyworks.com>
2020-02-16 14:11:57 -0600

    Add seq examples

--- src/collection/demo/whole_collection.clj
+++ src/collection/demo/whole_collection.clj
@@ -1,2 +1,19 @@
 (ns collection.demo.whole-collection)
 ;; Changing the whole collection
+
+;; seq
+
+(seq [:lollipop :chocolate-bar :mint])
+;; => (:lollipop :chocolate-bar :mint)
+(seq #{:peas :cucumber :zucchini})
+;; => (:peas :zucchini :cucumber)
+(seq {:phone 4 :camera 3 :gyroscope 2})
+;; => ([:phone 4] [:camera 3] [:gyroscope 2])
+
+;; Empty collection return nil
+(seq [])
+;; => nil
+(seq #{})
+;; => nil
+(seq {})
+;; => nil
