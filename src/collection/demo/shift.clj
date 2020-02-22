826562b477bba095318ce439afc74e5bca0f085d
Peter Schuck <peter@bendyworks.com>
2020-02-22 11:30:23 -0600

    add shift functions using cycle

--- src/collection/demo/shift.clj
+++ src/collection/demo/shift.clj
@@ -56,3 +56,41 @@
 ;; => ((:a :b :c :d :e :f :g :h :i :j :k :l) (:d :e :f :g :h :i :j :k :l :a :b :c))
 (partition (count alphabet) 9 alphabet alphabet)
 ;; => ((:a :b :c :d :e :f :g :h :i :j :k :l) (:j :k :l :a :b :c :d :e :f :g :h :i))
+
+(defn cycle-shift [n coll]
+  (let [column-size (count coll)
+        drop-count (mod (+ column-size n) column-size)]
+    (take column-size (drop drop-count (cycle coll)))))
+
+(cycle-shift 3 alphabet)
+;; => (:d :e :f :g :h :i :j :k :l :a :b :c)
+(cycle-shift -3 alphabet)
+;; => (:j :k :l :a :b :c :d :e :f :g :h :i)
+
+(take 3 alphabet)
+;; => (:a :b :c)
+(take 5 alphabet)
+;; => (:a :b :c :d :e)
+(take (+ (count alphabet) 3) alphabet)
+;; => (:a :b :c :d :e :f :g :h :i :j :k :l)
+
+(drop 3 alphabet)
+;; => (:d :e :f :g :h :i :j :k :l)
+(drop 5 alphabet)
+;; => (:f :g :h :i :j :k :l)
+(drop (+ (count alphabet) 3) alphabet)
+;; => ()
+
+(take 20 (cycle [:round :and :round :we :go]))
+;; => (:round :and :round :we :go :round :and :round :we :go :round :and :round :we :go :round :and :round :we :go)
+(take 8 (cycle [:round :and :round :we :go]))
+
+(take 20 (drop 2 (cycle [:round :and :round :we :go])))
+;; => (:round :we :go :round :and :round :we :go :round :and :round :we :go :round :and :round :we :go :round :and)
+(take 20 (drop 4 (cycle [:round :and :round :we :go])))
+;; => (:go :round :and :round :we :go :round :and :round :we :go :round :and :round :we :go :round :and :round :we)
+
+(take (count alphabet) (drop 3 (cycle alphabet)))
+;; => (:d :e :f :g :h :i :j :k :l :a :b :c)
+(take (count alphabet) (drop 9 (cycle alphabet)))
+  ;; => (:j :k :l :a :b :c :d :e :f :g :h :i)
