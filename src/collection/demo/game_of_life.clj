83fd1d3bce572c487b021987b209ee8df0a5bfb3
Peter Schuck <peter@bendyworks.com>
2020-02-26 20:05:03 -0600

    Show set functions and game of life over time

--- src/collection/demo/game_of_life.clj
+++ src/collection/demo/game_of_life.clj
@@ -1,4 +1,5 @@
-(ns collection.demo.game-of-life)
+(ns collection.demo.game-of-life
+  (:require [clojure.set :as cset]))
 
 (def board #{[0 0] [0 1] [1 0] [1 1] [2 1] [1 2]})
 
@@ -99,3 +100,71 @@
 ;; => #{[2 2] [0 0] [0 2] [2 0] [2 1] [1 2]}
 (frequencies [:a :a :b :a :a :b :b :c])
 ;; => {:a 4, :b 3, :c 1}
+
+(defn cell-changes [board steps]
+  (->>
+    (take steps (iterate frequencies-next-step board))
+    (partition 2 1)
+    (map
+      (fn [[current-cells next-cells]]
+        (let [common-cells (cset/intersection current-cells next-cells)
+              dying-cells (cset/difference current-cells common-cells)
+              baby-cells (cset/difference next-cells common-cells)]
+          {:dying dying-cells :baby baby-cells})))))
+
+(cell-changes board 6)
+(comment
+  ({:dying #{[1 0] [1 1] [0 1]}, :baby #{[2 2] [0 2] [2 0]}}
+   {:dying #{[0 0] [0 2] [2 0]}, :baby #{[1 0] [1 3] [3 1] [0 1]}}
+   {:dying #{[2 2] [2 1] [1 2]}, :baby #{[2 3] [0 2] [2 0] [3 2]}}
+   {:dying #{}, :baby #{}}
+   {:dying #{}, :baby #{}}))
+
+(take 6 (iterate frequencies-next-step board))
+(comment
+  (#{[0 0] [1 0] [1 1] [2 1] [1 2] [0 1]}
+   #{[2 2] [0 0] [0 2] [2 0] [2 1] [1 2]}
+   #{[2 2] [1 0] [1 3] [3 1] [2 1] [1 2] [0 1]}
+   #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]}
+   #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]}
+   #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]}))
+
+(take 6 (iterate (fn plus-2 [x] (+ x 2)) 1))
+;; => (1 3 5 7 9 11)
+
+(partition 2 1 (take 6 (iterate frequencies-next-step board)))
+(comment
+  ((#{[0 0] [1 0] [1 1] [2 1] [1 2] [0 1]} #{[2 2] [0 0] [0 2] [2 0] [2 1] [1 2]})
+   (#{[2 2] [0 0] [0 2] [2 0] [2 1] [1 2]} #{[2 2] [1 0] [1 3] [3 1] [2 1] [1 2] [0 1]})
+   (#{[2 2] [1 0] [1 3] [3 1] [2 1] [1 2] [0 1]} #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]})
+   (#{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]} #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]})
+   (#{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]} #{[1 0] [2 3] [1 3] [0 2] [2 0] [3 1] [3 2] [0 1]})))
+(partition 2 1 [:a :b :c :d :e :f])
+;; => ((:a :b) (:b :c) (:c :d) (:d :e) (:e :f))
+
+(let [current-cells #{[0 0] [1 0] [1 1] [2 1] [1 2] [0 1]}
+      next-cells #{[2 2] [0 0] [0 2] [2 0] [2 1] [1 2]}
+      common-cells (cset/intersection current-cells next-cells)
+      dying-cells (cset/difference current-cells common-cells)
+      baby-cells (cset/difference next-cells common-cells)]
+  {:common common-cells :dying dying-cells :baby baby-cells})
+;; => {:common #{[0 0] [2 1] [1 2]}, :dying #{[1 0] [1 1] [0 1]}, :baby #{[2 2] [0 2] [2 0]}}
+
+(cset/intersection #{:a :b :c} #{:c :d :e})
+;; => #{:c}
+(cset/intersection #{:a :b :c} #{:c :d :e} #{:e :f :g})
+;; => #{}
+(cset/intersection #{:a :b :c} #{:c :d :e :b} #{:e :f :b :g})
+;; => #{:b}
+
+(cset/difference #{:a :b :c} #{:b :c})
+;; => #{:a}
+(cset/difference #{:a :b :c} #{:d :e} #{:b :c})
+;; => #{:a}
+(cset/difference #{:a :b :c} #{:b :c} #{:d :e})
+;; => #{:a}
+
+(cset/difference #{:a :b :c} #{:a :b :c :d} #{:d :e})
+;; => #{}
+(cset/difference #{:a :b :c :d} #{:a :b :c} #{:d :e})
+;; => #{}
